21a36afcc9e94921ab5951d94b87839ff9f642b4
poVvisal <156397412+poVvisal@users.noreply.github.com>
2025-05-29 09:32:27 +0000

    1commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express()
+
+//USE PROXY SERVER TO REDIRECT THE INCOMMING REQUEST
+const httpProxy = require('http-proxy')
+const proxy = httpProxy.createProxyServer();
+
+//REDIRECT TO THE STUDENT MICROSERVICE
+app.use('/student', (req, res) => {
+    console.log("INSIDE API GATEWAY STUDENT ROUTE")
+    proxy.web(req, res, { target: 'http://localhost:5000' });
+})
+
+//REDIRECT TO THE TEACHER MICROSERVICE
+app.use('/teacher', (req, res) => {
+    console.log("INSIDE API GATEWAY TEACHER ROUTE")
+    proxy.web(req, res, { target: 'http://localhost:5001' });
+})
+
+app.listen(4000, () => {
+    console.log("API Gateway Service is running on PORT NO : ", 3000)
+})
